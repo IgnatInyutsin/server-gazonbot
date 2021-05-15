313d2a1b1cad65f46b9ddea7a158d5ef238493e8
IgnatInyutsin <iggyinyutsin@gmail.com>
2021-05-15 22:49:29 +0400

    database connect

--- flask/secondStage.py
+++ flask/secondStage.py
@@ -1,7 +1,15 @@
 import os
 from flask import Flask, jsonify, request
 from . import *
+import psycopg2
 
 #тут будут последующие действия
 secondStage(data):
-	retiutn "ВААААЙ МАЛАДЕССССС"
+	db = psycopg2.connect(
+		database="gazonbot_db",
+		user="gazonbot_user",
+		password="PaSsWoRd21",
+		host="127.0.0.1",
+		port="5432"
+	)
+	return "ВААААЙ МАЛАДЕССССС"
