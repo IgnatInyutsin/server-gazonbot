9fc1f0f7c796232aab57e906f519f49fb4d6611d
IgnatInyutsin <iggyinyutsin@gmail.com>
2021-05-15 11:05:59 +0400

    flask update

--- flask/app.py
+++ flask/app.py
@@ -1,13 +1,58 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-#we define the route /
-@app.route('/')
-def welcome():
-    # return a json
-    return jsonify({'status': 'api working'})
+#функция handler срабатывает, если запрос идет на корень
+@app.route('/', methods=['GET'])
+def handler():
+	lis1 = []
+	int1 = request.form['ID']
+	int2 = 0
+	lis2 = []
+	for i in range(0, len(int1)):
+    	if int1[i] == '0':
+        	lis1.append(0)
+    	elif int1[i] == '1':
+        	lis1.append(1)
+    	elif int1[i] == '2':
+        	lis1.append(2)
+    	elif int1[i] == '3':
+        	lis1.append(3)
+    	elif int1[i] == '4':
+        	lis1.append(4)
+    	elif int1[i] == '5':
+        	lis1.append(5)
+    	elif int1[i] == '6':
+        	lis1.append(6)
+    	elif int1[i] == '7':
+        	lis1.append(7)
+    	elif int1[i] == '8':
+        	lis1.append(8)
+    	elif int1[i] == '9':
+        	lis1.append(9)
+    	elif int1[i] == 'A':
+        	lis1.append(10)
+    	elif int1[i] == 'B':
+        	lis1.append(11)
+    	elif int1[i] == 'C':
+        	lis1.append(12)
+    	elif int1[i] == 'D':
+        	lis1.append(13)
+    	elif int1[i] == 'E':
+        	lis1.append(14)
+    	elif int1[i] == 'F':
+        	lis1.append(15)
+    	elif int1[i] == 'G':
+        	lis1.append(16)
+
+        lis1.reverse()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    for i in range(0, len(lis1)):
+        int2 = int2 + lis1[i]*(17**i)
+
+    if int2 % 3 == 0:
+        #тут будут какие то действия
+        return "Вай какой МаЛаДеЦ ЧисТыЙ, Не ХокЕЕрИт"
+    else:
+        return "ПАШЕЛ НАХУЙ ГРЯЗНЫЙ ХАКЕР ЫАЫАЫА"
